c9ff60a857af64ffe7666666b0f4f06a71c23f6d
kadhir-glitch <mdkadhirbasha@gmail.com>
2024-12-09 21:58:05 +0530

    Add files via upload

---
+++ Iphone.java
@@ -0,0 +1,56 @@
+package applePhone;
+
+public class Iphone {
+			 
+		 
+	public void apple13(){
+		System.out.println("apple13");
+	}
+	public void macpad() {
+		System.out.println("macpad");
+	}
+	public void appleTv() {
+		System.out.println("appleTv");
+	}
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		 String nam = "riswan";
+		char initial ='n';
+		 int age =19;
+		 long ph =5456101l;
+		float cgp = 7.2f;
+		boolean result = true;
+		
+		System.out.println(nam);
+		System.out.println(initial);
+		System.out.println(age);
+		System.out.println(ph);
+		System.out.println(cgp);
+		System.out.println(result);
+		
+		
+		Iphone obj =new Iphone();
+			obj.apple13();	
+		obj.appleTv();
+		obj.macpad();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
